cd5cec5a7b190af03b50fcc1ec88421d004ce318
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-09 01:49:23 +0530

    Added login form and navbar buttons for classroom

---
+++ src/components/ClassArray.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const ClassArray = [
+    {
+      id: '1 - Male',
+      name: 'Male'    
+    },
+    {
+      id: '2 - Female',
+      name: 'Female'    
+    },
+    {
+      id: '3 - Other',
+      name: 'Other'    
+    },
+  ];
