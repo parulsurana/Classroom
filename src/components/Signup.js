83ff0776c8c7d8d1aaa7e8d11e19af8aede9e202
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-21 01:01:26 +0530

    sign

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,88 +2,114 @@ import "./Signup.css";
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Button from "./Button";
+import { useForm } from "react-hook-form";
 
 
 export default function Register({ Education, Section, instructor, student}) {
-  const history = useHistory();
-  const [userName, setUserName] = useState("");
-  const [education, setEducation] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [gender, setGender] = useState("");
-  const [section, setSection] = useState("");
 
-  const signUp = e => {
-    e.preventDefault()
-    history.push('/')
-  }
+  const {register, handleSubmit, watch, error} = useForm();
+  // const history = useHistory();
+  // const [userName, setUserName] = useState("");
+  // const [education, setEducation] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [gender, setGender] = useState("");
+  // const [section, setSection] = useState("");
+
+  // const signUp = e => {
+  //   e.preventDefault()
+  //   history.push('/home');
+  // }
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="register-page">
-      <form className="register-box">
+      <form className="register-box" onSubmit={handleSubmit(onSubmit)}>
         <h2>Create Account</h2>
         <div className="user-box">
            <label>UserName</label>
            <input
+           name="username"
             placeholder="UserName"
             type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            ref={register({required: true})}
+            // value={userName}
+            // onChange={(e) => setUserName(e.target.value)}
           ></input>
         </div>
 
         <div className="user-box">
           <label>Gender</label>
           <input
+            name="gender"
             placeholder="Gender"
             type="text"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
+            ref={register({required: true})}
+
+            // value={gender}
+            // onChange={(e) => setGender(e.target.value)}
           ></input>
         </div>
 
         <div className="user-box">
           {Education && <label>Education</label>}
           {Education && <input
+            name="education"
             placeholder="Education"
             type="text"
-            value={education}
-            onChange={(e) => setEducation(e.target.value)}
+            ref={register({required: true})}
+
+            // value={education}
+            // onChange={(e) => setEducation(e.target.value)}
           ></input>}
         </div>
 
         <div className="user-box">
           {Section && <label>Section</label>}
           {Section && <input
+            name="section"
             placeholder="Section"
             type="text"
-            value={section}
-            onChange={(e) => setSection(e.target.value)}
+            ref={register({required: true})}
+
+            // value={section}
+            // onChange={(e) => setSection(e.target.value)}
           ></input>}
         </div>
 
         <div className="user-box">
           <label>Email</label>
           <input
+            name="email"
             placeholder="Email"
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            ref={register({required: true})}
+
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
           ></input>
         </div>
 
         <div className="user-box">
           <label>Password</label>
           <input
+            name="password"
             placeholder="Password"
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            ref={register({required: true})}
+
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
           ></input>
         </div>
 
-        <div className="user-box">
-          <Button buttonName="submit" label="Sign up" onClick={signUp} />
+
+        {/* onClick={signUp} */}
+        <div className="user-box" >                         
+          <Button buttonName="submit" label="Sign up"/>
         </div>
 
         <div className="user-box">
