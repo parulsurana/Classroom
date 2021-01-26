bbfb644d2d7d5ff983c3598811e1ba73124f8734
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-27 01:54:38 +0530

    Styling to pop ups

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,15 +3,20 @@ import React from 'react'
 
 export default function Input(props) {
 
-    const {name,label,value,error=null,onChange} = props;
+    const {id,name,label,value,error=null,onChange,type,url} = props;
     return (
         <TextField
         variant="outlined"
         label={label}
         name={name}
         value={value}
+        type={type}
         onChange={onChange}
+        url={url}
+        id={id}
+        multiline
         {...(error && {error:true, helperText:error })}
     />
+   
     )
 }
