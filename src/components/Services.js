711380328dd35307eaa99b3fa0cd0ac000e5b312
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-31 14:22:13 +0530

    css change

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,41 +1,11 @@
 export const GetSectionCollection = [
-    { id: '1', title: 'Class I' },
-    { id: '2', title: 'Class II' },
-    { id: '3', title: 'Class III' },
-    { id: '4', title: 'Class IV' },
-    { id: '5', title: 'Class V' },
-    { id: '6', title: 'Class VI' },
-    { id: '7', title: 'Class VII' },
-    { id: '8', title: 'Class VIII' },
-    { id: '9', title: 'Class IX' },
-    { id: '10', title: 'Class X' },
+    { id: '1', title: 'Computer Science' },
+    { id: '2', title: 'Information Technology' },
+    { id: '3', title: 'Electronics and Communication Engineering' },
+    { id: '4', title: 'Mechanical Engineering' },
+    { id: '5', title: 'Civil Engineering' },
+    { id: '6', title: 'Chemical Engineering' },
+    { id: '7', title: 'Biochemical / Biomedical Engineering' },
+    { id: '8', title: 'Aerospace / Aeronautical Engineering' },
 ];
 
-export default function employeeServices(){
-const KEYS = {
-    employees: "employees",
-    employeeId: "employeeId",
-}
-
-
- function insertEmployee(data) {
-    let employees = getAllEmployees();
-    data['id'] = generateEmployeeId();
-    employees.push(data)
-    localStorage.setItem(KEYS.employees, JSON.stringify(employees))
-}
-
- function generateEmployeeId() {
-    if (localStorage.getItem(KEYS.employeeId) == null)
-        localStorage.setItem(KEYS.employeeId, "0")
-    var id = parseInt(localStorage.getItem(KEYS.employeeId))
-        localStorage.setItem(KEYS.employeeId, (++id).toString());
-    return id;
-}
-
- function getAllEmployees() {
-    if (localStorage.getItem(KEYS.employees) == null)
-        localStorage.setItem(KEYS.employees, JSON.stringify([]))
-    return JSON.parse(localStorage.getItem(KEYS.employees));
-}
-}
